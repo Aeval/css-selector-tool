45c8194e9bb40095fcfd2fd833e9c238a7021c47
Harry C <hchilingrian@thorlabs.com>
2019-02-04 16:01:29 -0500

    Pat helped
    the plugin now generates a page that, if I can get it into a file and link said file to excel, will be perfect for the "details". section.

--- code/background.js
+++ code/background.js
@@ -2,5 +2,7 @@ chrome.browserAction.onClicked.addListener(function (tab) {
     chrome.tabs.executeScript(null, { file: "jquery-2.2.min.js" });
     chrome.tabs.executeScript(null, { file: "jquery-ui.min.js" });
     chrome.tabs.executeScript(null, { file: "html2canvas.min.js" });
+    chrome.tabs.executeScript(null, { file: "html2canvas.js" });
     chrome.tabs.executeScript(null, { file: "nScript.js" });
+    //chrome.tabs.executeScript(null, { file: "html2canvasFunction.js" });
 });
--- code/nScript.js
+++ code/nScript.js
@@ -1,172 +1,52 @@
-/*
-var nd___vo = {
-  remove: function () {
-    nd___vo.clean();
-    $("#nd___box").hide();
-    $("#nd___cache").text($.trim($("#nd___selector").val()));
-  },
-  show: function () {
-    $("#nd___box").show();
-    var q = $.trim($("#nd___selector").val());
-    if (!q) {
-      q = $("#nd___cache").text();
-      $("#nd___selector").val(q);
-    }
-    if (q) {
-      nd___vo.go();
-    }
-  },
-  add: function () {
-    $("body").append("<div id='nd___box'></div>");
-    $("#nd___box").append("<button id='nd___close'>X</button><span id='nd___title'>CSS Selector Tool</span>");
-    $("#nd___box").append("<form action='#' id='nd___form'></form>");
-    $("#nd___form").append("<p><input type='text' id='nd___selector' placeholder='Selector' /></p><p><a id='nd___clean'>Clear Selector</a></p>");
-    $("#nd___form").append("<p>Element: <span id='nd___status'>undefined</span></p>");
-    $("#nd___form").append("<p class='checkbox'><input type='checkbox' id='nd___ohighlight' checked='true' /><label for='nd___ohighlight'>Highlight</label></p>");
-    $("#nd___box").append("<button id='genPage'>Generate</button>");
-  },
-  observe: function() {
-    $("#nd___form").on("submit", function(){ return false; });
-    $("#nd___selector").keyup(function(){nd___vo.go();});
-    $("#nd___box").draggable({ opacity: 0.35 }).resizable({ minWidth: 305 , minHeight: 175 });
-    $("#nd___clean").click(function(){
-      nd___vo.alert("undefined");
-      nd___vo.clean();
-      $("#nd___selector").val("");
-      nd___vo.cacheE = null;
-    });
-    $("#nd___close").click(function () {
-      nd___vo.alert("undefined");
-      nd___vo.remove();
-      $("#nd___selector").val("");
-    });
-    $('#nd___ohighlight').change(function () {
-      if (!this.checked) {
-        nd___vo.clean();
-      } else {
-        if (nd___vo.cacheE != null) {
-          $(nd___vo.cacheE).css("outlineColor", "RGB(204,0,0)");
-          $(nd___vo.cacheE).css("outlineStyle", "dotted");
-          $(nd___vo.cacheE).css("outlineWidth", "2px");
-          $(nd___vo.cacheE).addClass("nd___highlighted");
-        }
-      }
-    });
-  },
-  go: function () {
-    nd___vo.clean();
-    var q, e;
-    q = $.trim($("#nd___selector").val());
-    if (q === "#" || q === "." || q === "HTML" || q === "*" || q === "") {
-      return false;
-    }
-    //e = $("*:not(div#nd___box " + q + ")" + q); FAILS to get cascade.
-    try {
-      e = $(q);
-    } catch (error) {
-      nd___vo.alert("Invalid selector: " + q);
-      return false;
-    }
-    if (e[0]) {
-      e = $.grep(e, function (domEle) {
-        if ($(domEle).attr("id") === "nd___box" || ($(domEle).parents("#nd___box")[0])) {
-          return false;
-        }
-        return true;
-      });
-      if (e.length < 1) {
-        return false;
-      }
-      nd___vo.cacheOC = $(e).css("outlineColor");
-      nd___vo.cacheOS = $(e).css("outlineStyle");
-      nd___vo.cacheOW = $(e).css("outlineWidth");
-
-      nd___vo.alert(nd___vo.info(e));
-      nd___vo.cacheE = e;
-      if ($("#nd___ohighlight")[0].checked) {
-        $(e).css("outlineColor", "RGB(204,0,0)");
-        $(e).css("outlineStyle", "dashed");
-        $(e).css("outlineWidth", "2px");
-        $(e).addClass("nd___highlighted");
-      }
-    } else {
-      nd___vo.alert("undefined");
-    }
-  },
-  alert: function (msg) {
-    $("#nd___status").html(msg);
-  },
-  clean: function () {
-    if (nd___vo.cacheE != null) {
-      $(nd___vo.cacheE).css("outlineColor", nd___vo.cacheOC);
-      $(nd___vo.cacheE).css("outlineStyle", nd___vo.cacheOS);
-      $(nd___vo.cacheE).css("outlineWidth", nd___vo.cacheOW);
-      $(nd___vo.cacheE).removeClass("nd___highlighted");
-
-    }
-  },
-  info: function (ele) {
-    if (!$(ele)[0]) {
-      return false;
-    }
-    var count = $(ele).length - 1;
-    var sel = $(ele)[0].tagName;
-    var id = $(ele).attr("id");
-    if (id) {
-      sel += "#" + id;
-    }
-    var cN = $(ele).attr("class");
-    if (cN) {
-      sel += "." + $.trim(cN).replace(/\s/gi, ".");
-    }
-    if (count > 0) {
-      sel += ", " + count + " others...";
-    }
-    return sel;
-  }
-};
-if ($("#nd___box")[0]) {
-  nd___vo.show();
-} else {
-  nd___vo.add();
-  nd___vo.observe();
-}
 
 
-var desiredTextOne = document.getElementById('tab-main').innerHTML;
-var desiredTextTwo = document.getElementById('tab-headers').innerHTML;
-var desiredTextThree = document.getElementById('tab-cookies').innerHTML;
-var desiredTextFour = document.getElementById('tab-jdbc').innerHTML;
-var desiredTextFive = document.getElementById('tab-properties').innerHTML;
-var desiredTextSix = document.getElementById('tab-relations').innerHTML;
-var desiredTextSeven = document.getElementById('tab-errors').innerHTML;
 
 
-var html = "<div id='chart1'>" + desiredTextOne + "</div>";
-html += "<div id='chart2'>" + desiredTextTwo + "</div>";
-html += "<div id='chart3'>" + desiredTextThree + "</div>";
-html += "<div id='chart4'>" + desiredTextFour + "</div>";
-html += "<div id='chart5'>" + desiredTextFive + "</div>";
-html += "<div id='chart6'>" + desiredTextSix + "</div>";
-html += "<div id='chart7'>" + desiredTextSeven + "</div>";
+var documentHTML = $('<div>');
+$(documentHTML).html($('html').html());
 
-$('#genPage').on('click', function() {
-  var newWindow = window.open();
 
-  newWindow.document.write(html);
+$('.pre-scrollable', documentHTML).each(function(){
+  console.log(this)
+  $(this).removeClass('pre-scrollable');
 });
-*/
+
+$(documentHTML).find('script').remove();
+$(documentHTML).find('#frsidemenu').remove();
+$(documentHTML).find('.navbar').remove()
+$('div.tab-content', documentHTML).removeClass('tab-content');
+
+var newWindow = window.open();
+
+newWindow.document.write($(documentHTML).html());
+
+
+/*
+//var headContent = document.getElementsByTagName('head')[0].innerHTML;
+
 
 
 var headContent = document.getElementsByTagName('head')[0].innerHTML;
 var desiredText = document.getElementsByClassName('tab-content')[0].innerHTML;
-console.log(headContent)
 
 var newWindow = window.open();
 
-newWindow.document.write('<head>' + headContent + '</head>');
+newWindow.document.write('<head>' + headContent + '</head');
 newWindow.document.write('<body>' + '<div id="data">' + desiredText + '</div>' + '</body>');
 
+//document = newWindow.document;
+$(newWindow.document).ready(function(){
+  var _that = this;
+  $('.pre-scrollable', _that).each(function(){console.log(this)
+    $(this).removeClass('pre-scrollable');
+  });
+})
 
 
-//newWindow.document.write(desiredText);
+
+/*
+//html2canvas OGcode
+html2canvas(newWindow.document.getElementById('data')).then(function(canvas) {
+  newWindow.appendChild(canvas);
+})
+*/
