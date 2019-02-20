2022e3eddd979b6dfab6dca7ede04c5967a9ada7
Harry C <hchilingrian@thorlabs.com>
2019-02-20 09:07:33 -0500

    2/20 AM

--- code/nScript6.js
+++ code/nScript6.js
@@ -1,4 +1,4 @@
 var desiredText = document.getElementsByClassName('tab-content')[0].innerHTML;
 var newWindow = window.open();
-newWindow.document.write('<head><link rel="stylesheet" type="text/css" href="nStyles2.css"></head>');
-newWindow.document.write(desiredText);
+newWindow.document.write('<link rel="stylesheet" href="nStyles2.css" type="text/css">' + desiredText);
+//newWindow.document.write('<head><style>body {  background-color: linen;}h1 {  color: maroon;  margin-left: 40px;}</style></head> ' + desiredText);
