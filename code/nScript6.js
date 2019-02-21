97dfbec17f9cca7d569e7b2107e547a72a832444
Harry C <hchilingrian@thorlabs.com>
2019-02-21 15:44:35 -0500

    2/21 midday

--- code/nScript6.js
+++ code/nScript6.js
@@ -1,12 +1,12 @@
 var desiredText = document.getElementsByClassName('tab-content')[0].outerHTML;
-//var newWindow = window.open();
-var summary = $('#details_table [data-title="ID /Thread"] span').html();
-
-console.log(summary);
-//newWindow.document.write('<head> <link rel="stylesheet" type="text/css" href="nStyles.css" /> </head>' + desiredText);
-
-/*
-
-//newWindow.document.write('<head><style>body {  background-color: linen;}h1 {  color: maroon;  margin-left: 40px;}</style></head> ' + desiredText);
-
-*/
+var summaryID = 'ID: ' + $('* > td:nth-child(4) > span:nth-child(1)').html();
+//var userFlag = 'User: ' + $('*#headers_table > tbody > tr:nth-child(8) > td:nth-child(2) > span').html();
+var userFlag;
+var mozillaCheck = $(desiredText).text().indexOf('Godzilla') > -1;
+if (mozillaCheck = true) {userFlag = 'User Affected'} else {userFlag = 'Bot Issue'};
+var errorText = 'Error: ' + $('div#tab-errors > .section.clearfix.span12 > .pre-scrollable').html();
+var output = summaryID + '<br>' + userFlag + '<br>' + errorText;
+//console.log(errorText)
+var newWindow = window.open();
+newWindow.document.write(output);
+newWindow.document.write('<head> <link rel="stylesheet" type="text/css" href="nStyles.css" /> </head>' + desiredText);
