589c59dca716f81585bc046960d17c5c525f0d18
Harry C <hchilingrian@thorlabs.com>
2019-02-22 10:20:21 -0500

    massive cutdown

--- code/nScript.js
+++ code/nScript.js
@@ -1,42 +1,40 @@
-var documentHTML = $('<div>');
-$(documentHTML).html($('html').html());
-$('.pre-scrollable', documentHTML).each(function () {
-  $(this).removeClass('pre-scrollable');
-});
-//$(documentHTML).find('script').remove();
-$(documentHTML).find('#frsidemenu').remove();
-$(documentHTML).find('.navbar').remove()
-$('div.tab-content', documentHTML).removeClass('tab-content');
-//$('#details_table', documentHTML).unwrap("<div class='details_table'></div>");
-//$('#details_table', documentHTML).wrap("<div class='imgTab'></div>");
-$('[id^=tab-]', documentHTML).wrap("<div class='imgTab'></div>");
-$('[id$=errors]', documentHTML).unwrap("<div class='imgTab'></div>");
-$('[id$=errors]', documentHTML).wrap("<div class='textTab'></div>");
-var newWindow = window.open();
-var newWindowTwo = window.open();
+var desiredText = document.getElementsByClassName('tab-content')[0].outerHTML;
+var serverName;
+var pathName = window.location.href; 
+if (pathName.includes("10.75.3.37")) {
+    serverName = 'JPwebnode01AZ';
+}
+else if (pathName.includes("172.16.15.81")) {
+    serverName = 'SHAwebnode01';
+}
+else if (pathName.includes("10.75.7.36")) {
+    serverName = 'UKwebnode01AZ';
+}
+else if (pathName.includes("10.75.5.36")) {
+    serverName = 'USwebnode01AZ';
+}
+else if (pathName.includes("10.75.6.36")) {
+    serverName = 'USwebnode02AZ';
+}
+else if (pathName.includes("10.1.2.31")) {
+    serverName = 'USwebnode03';
+}
+else {
+    serverName = 'An error has occured'
+}
+var summaryID = 'ID: ' + $('* > td:nth-child(4) > span:nth-child(1)').html();
+var userFlag;
+var mozillaCheck = $(desiredText).text().indexOf('Mozilla') > -1;
+if (mozillaCheck == true) {
+    userFlag = 'User Affected';
+} else {
+    userFlag = 'Bot Issue';
+}
+var errorLocation = 'Error Location: ' + $('#request_table > tbody > tr:nth-child(8) > td:nth-child(2) > span').html();
+var errorText = 'Error Type: ' + $('div#tab-errors > .section.clearfix.span12 > .pre-scrollable').html();
+var output = serverName + '<br>' + summaryID + '<br>' + userFlag + '<br>' + errorLocation + '<br>' + errorText + '<hr>' + desiredText;
 
-newWindow.document.write($(documentHTML).html());
-$('#sect_details_table', newWindow.document).hide();
-$('#controls_top', newWindow.document).hide();
+//var newWindow = window.open();
+//newWindow.document.write(output);
 
-$('.tab-pane', newWindow.document).hide();
-$('.tab-pane', documentHTML).each(function () {
-  $(this).click();
-  html2canvas(newWindow.document.body).then(function(canvas) {
-    newWindowTwo.document.body.appendChild(canvas);
-  });
-    //$('.tab-pane', newWindow.document).hide();
-});
-
-
-/*
-$('.tab-pane', newWindow.document).hide();
-$('.tab-pane', documentHTML).each(function () {
-  $(this).show();
-  html2canvas(newWindow.document.body).then(function(canvas) {
-    newWindowTwo.document.body.appendChild(canvas);
-  });
-    $('.tab-pane', newWindow.document).hide();
-});
-*/
 
