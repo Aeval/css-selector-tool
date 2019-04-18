ecf6779544fd43245ef2a7917bbf5b4ce15291b0
Harry C <hchilingrian@thorlabs.com>
2019-04-18 08:59:22 -0400

    Update background.js

--- code/background.js
+++ code/background.js
@@ -1,6 +1,5 @@
 chrome.browserAction.onClicked.addListener(function (tab) {
     chrome.tabs.executeScript(null, { file: "jquery-2.2.min.js" });
     chrome.tabs.executeScript(null, { file: "jquery-ui.min.js" });
-    chrome.tabs.executeScript(null, { file: "clipboard.min.js" });
     chrome.tabs.executeScript(null, { file: "nScript.js" });
 });
